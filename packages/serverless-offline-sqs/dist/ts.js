a7465ee2721c58b632283b47bee25e53b6168546
Nosekim <rogerfelipensk@gmail.com>
2022-10-08 01:56:14 -0300

    fiz build

---
+++ packages/serverless-offline-sqs/dist/ts.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _lambda = require("serverless-offline/lambda");
+
+var _lambda2 = _interopRequireDefault(_lambda);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log("teste");
